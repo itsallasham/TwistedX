f2cd2dd5f01e7c71623f64047608b0fed73cdb05
vork <itsallasham@gmail.com>
2015-01-04 00:03:08 -0600

    basic pretty

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,16 @@
 
 
 /* GET home page */
-exports.index = function(req, res){
-res.render('index', { title: 'Express' });
+exports.home = function(req, res){
+res.render('home', { title: 'Twisted-X Boots' });
 };
 
+/*GET catalog page*/
+exports.catalog = function(req, res) {
+	res.render('index', { title: 'Catalog'});
+};
+
+/*GET sponsorships page*/
+exports.sponsorships = function(req, res) {
+	res.render('index', { title: 'sponsorships'});
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var ctrl = require('../app_server/controllers/main');
 
-module.exports = function(app) {
-	app.get('/', ctrl.index);
+module.exports = function(app){
+	require('./main')(app);
+
 };
